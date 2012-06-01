b0d7ea7d0f14b273ab8895d5e8bbf84df98a30e4
Simone Padovan <simone.padovan@comune.bolzano.it>
2012-06-01 15:42:21 +0200

    Spaziatura del codice dei controller

--- grails-app/controllers/it/bolzano/comune/bauamtWeb/BaseController.groovy
+++ grails-app/controllers/it/bolzano/comune/bauamtWeb/BaseController.groovy
@@ -4,77 +4,77 @@ import grails.converters.*
 
 abstract class BaseController {
 	
-	static allowedMethods = [list: ['GET', 'POST'], show: "GET"]
+    static allowedMethods = [list: ['GET', 'POST'], show: "GET"]
 
-	def model
+    def model
 	
-	protected def getDefaultDateFormat() {
-	  message(code: "default.date.format", default: 'dd/MM/yyyy')
-	}  
+    protected def getDefaultDateFormat() {
+        message(code: "default.date.format", default: 'dd/MM/yyyy')
+    }  
 	 
-	protected def getModelName() {
-		model.metaClass.theClass.toString().tokenize('.').last()
-	}
+    protected def getModelName() {
+        model.metaClass.theClass.toString().tokenize('.').last()
+    }
 	
-	protected def uncapitalize (String value) {
-		value[0].toLowerCase() + value.substring(1)
-	}
+    protected def uncapitalize (String value) {
+        value[0].toLowerCase() + value.substring(1)
+    }
   
-	protected def getModelLabel() {
-		uncapitalize(getModelName()) + '.label'
-	}	
+    protected def getModelLabel() {
+        uncapitalize(getModelName()) + '.label'
+    }	
 
-	protected def getModelXmlKey() {
-		uncapitalize(getModelName())
-	}
+    protected def getModelXmlKey() {
+        uncapitalize(getModelName())
+    }
 	
-	protected def getInstanceListKey() {
-		uncapitalize(getModelName()) + 'InstanceList'
-	}
+    protected def getInstanceListKey() {
+        uncapitalize(getModelName()) + 'InstanceList'
+    }
 	
-	protected def getInstanceTotalKey() {
-		uncapitalize(getModelName()) + 'InstanceTotal'
-	}
+    protected def getInstanceTotalKey() {
+        uncapitalize(getModelName()) + 'InstanceTotal'
+    }
 
-	protected def getInstanceKey() {
-		uncapitalize(getModelName()) + 'Instance'
-	}
+    protected def getInstanceKey() {
+        uncapitalize(getModelName()) + 'Instance'
+    }
 	
-	def index() {
-		redirect(action: "list", params: params)
-	}
+    def index() {
+        redirect(action: "list", params: params)
+    }
 	
-	protected def doList(params) {
-		model.list(params)
-	}
+    protected def doList(params) {
+        model.list(params)
+    }
 
-	def list() {
-		params.max = Math.min(params.max ? params.int('max') : 10, 100)				
-		def retvalue = doList(params)
-		def tot = (retvalue ? retvalue?.getTotalCount() : 0) 
+    def list() {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)				
+        def retvalue = doList(params)
+        def tot = (retvalue ? retvalue?.getTotalCount() : 0) 
 		
-		withFormat {
-			html {[(getInstanceListKey()): retvalue, (getInstanceTotalKey()): tot]}
-			xml { render retvalue as XML }
-			json { render retvalue as JSON }
-		}
-	}
+        withFormat {
+            html {[(getInstanceListKey()): retvalue, (getInstanceTotalKey()): tot]}
+            xml { render retvalue as XML }
+            json { render retvalue as JSON }
+        }
+    }
 
-	protected def doFind(params) {
-		model.findById(params.id)
-	}	
+    protected def doFind(params) {
+        model.findById(params.id)
+    }	
 	
-	def show() {
-		def retvalue
-		if(params.id && model.exists(params.id)){
-			retvalue = doFind(params)
-		}
-		if (retvalue) {
-			withFormat {
-				html { [(getInstanceKey()): retvalue] }
-				xml { render retvalue as XML }
-				json { render retvalue as JSON }
-			}
-		}
-	}
+    def show() {
+        def retvalue
+        if(params.id && model.exists(params.id)){
+            retvalue = doFind(params)
+        }
+        if (retvalue) {
+            withFormat {
+                html { [(getInstanceKey()): retvalue] }
+                xml { render retvalue as XML }
+                json { render retvalue as JSON }
+            }
+        }
+    }
 }
--- grails-app/controllers/it/bolzano/comune/bauamtWeb/BauakteController.groovy
+++ grails-app/controllers/it/bolzano/comune/bauamtWeb/BauakteController.groovy
@@ -2,154 +2,154 @@ package it.bolzano.comune.bauamtWeb
 
 class BauakteController extends BaseController {
 
-	// Imposto model di BaseController
+    // Imposto model di BaseController
     def beforeInterceptor = {
-		model = Bauakte
-	}
+        model = Bauakte
+    }
 
-	def reset() {
-		params.clear()
-		render (view: "list", model: list())		
-	}
+    def reset() {
+        params.clear()
+        render (view: "list", model: list())		
+    }
 	
-	protected def doList(params) {
-		if (!params.tipoPratica && !params.tipoOpera && !params.baNummer && !params.archivnr &&
-			!params.daDataPresentazione && !params.aDataPresentazione && 
-			!params.daDataDeterminazione && !params.aDataDeterminazione &&
-			!params.sedeOpera &&
-			!params.comuneCatastale && !params.cognomeProgettista && !params.cognomeRichiedente) {
- 		  flash.message = "${message(code: 'almenouno.messaggio', default: 'Mindenstens eine option auswaehlen')}"
-		  return null
-		}  
-		flash.message = null
+    protected def doList(params) {
+        if (!params.tipoPratica && !params.tipoOpera && !params.baNummer && !params.archivnr &&
+            !params.daDataPresentazione && !params.aDataPresentazione && 
+            !params.daDataDeterminazione && !params.aDataDeterminazione &&
+            !params.sedeOpera &&
+            !params.comuneCatastale && !params.cognomeProgettista && !params.cognomeRichiedente) {
+            flash.message = "${message(code: 'almenouno.messaggio', default: 'Mindenstens eine option auswaehlen')}"
+            return null
+        }  
+        flash.message = null
 					
-		Bauakte.createCriteria().list(max:params.max, offset:params.offset) {
-			if (params.tipoPratica && params.tipoPratica.isNumber())
-			  eq("verwaltungsaktId", new Long(params.tipoPratica))
+        Bauakte.createCriteria().list(max:params.max, offset:params.offset) {
+            if (params.tipoPratica && params.tipoPratica.isNumber())
+            eq("verwaltungsaktId", new Long(params.tipoPratica))
 			
-			if (params.tipoOpera && params.tipoOpera.isNumber())
-			  eq("artId", new Long(params.tipoOpera))
+            if (params.tipoOpera && params.tipoOpera.isNumber())
+            eq("artId", new Long(params.tipoOpera))
 			
-			if (params.baNummer)
-			  eq("baNummer", params.baNummer)
+            if (params.baNummer)
+            eq("baNummer", params.baNummer)
 
-			if (params.archivnr)
-			  eq("archivnr", params.archivnr)
+            if (params.archivnr)
+            eq("archivnr", params.archivnr)
 
 			  			
-			if (params.daDataPresentazione)
-			   gte("eingangsdatum", params.date('daDataPresentazione', getDefaultDateFormat()))
+            if (params.daDataPresentazione)
+            gte("eingangsdatum", params.date('daDataPresentazione', getDefaultDateFormat()))
 
-		    if (params.aDataPresentazione)
-			   lte("eingangsdatum", params.date('aDataPresentazione', getDefaultDateFormat()))			   
+            if (params.aDataPresentazione)
+            lte("eingangsdatum", params.date('aDataPresentazione', getDefaultDateFormat()))			   
 
-			if (params.daDataDeterminazione && params.date('daDataDeterminazione', getDefaultDateFormat())) {
-			   def restriction = "exists (select dokumentnr from detail_dokumente doc where doc.ba_id = {alias}.ba_id AND doc.dokumentdatum >= TO_DATE('$params.daDataDeterminazione', '"+getDefaultDateFormat()+"'))"
-			   sqlRestriction restriction
+            if (params.daDataDeterminazione && params.date('daDataDeterminazione', getDefaultDateFormat())) {
+                def restriction = "exists (select dokumentnr from detail_dokumente doc where doc.ba_id = {alias}.ba_id AND doc.dokumentdatum >= TO_DATE('$params.daDataDeterminazione', '"+getDefaultDateFormat()+"'))"
+                sqlRestriction restriction
 			   
-			   /*
-			   documenti {
-			      gte("dokumentdatum", params.date('daDataDeterminazione', getDefaultDateFormat()))
-			   }
-			   */	  
-			}   
+                /*
+                documenti {
+                gte("dokumentdatum", params.date('daDataDeterminazione', getDefaultDateFormat()))
+                }
+                 */	  
+            }   
 
-			if (params.aDataDeterminazione && params.date('aDataDeterminazione', getDefaultDateFormat())) {
-			   def restriction = "exists (select dokumentnr from detail_dokumente doc where doc.ba_id = {alias}.ba_id AND doc.dokumentdatum <= TO_DATE('$params.aDataDeterminazione', '"+getDefaultDateFormat()+"'))"
-			   sqlRestriction restriction
+            if (params.aDataDeterminazione && params.date('aDataDeterminazione', getDefaultDateFormat())) {
+                def restriction = "exists (select dokumentnr from detail_dokumente doc where doc.ba_id = {alias}.ba_id AND doc.dokumentdatum <= TO_DATE('$params.aDataDeterminazione', '"+getDefaultDateFormat()+"'))"
+                sqlRestriction restriction
 				
-			   /*
-			   documenti {
-			      lte("dokumentdatum", params.date('aDataDeterminazione', getDefaultDateFormat()))
-			   }
-			   */	  
-			}   
+                /*
+                documenti {
+                lte("dokumentdatum", params.date('aDataDeterminazione', getDefaultDateFormat()))
+                }
+                 */	  
+            }   
 			   
-   			if (params.sedeOpera && params.sedeOpera.isNumber()) {
-				def restriction = "exists (select id from detail_Indirizzo ind where ind.ba_id = {alias}.ba_id AND ind.cviacodi = $params.sedeOpera"
-				if (params.civico && params.civico.isInteger())
-  				   restriction += " AND ind.ncivnume = $params.civico"
-			    if (params.barra && params.barra.isNumber())
-			       restriction += " AND ind.ncivbarr = '${params.barra}'".toString()
-				restriction += ")"
+            if (params.sedeOpera && params.sedeOpera.isNumber()) {
+                def restriction = "exists (select id from detail_Indirizzo ind where ind.ba_id = {alias}.ba_id AND ind.cviacodi = $params.sedeOpera"
+                if (params.civico && params.civico.isInteger())
+                restriction += " AND ind.ncivnume = $params.civico"
+                if (params.barra && params.barra.isNumber())
+                restriction += " AND ind.ncivbarr = '${params.barra}'".toString()
+                restriction += ")"
 				   
-			    sqlRestriction restriction 			
+                sqlRestriction restriction 			
 
-		  /*		   
-		      indirizzo {
-				   eq('cviacodi', new Long(params.sedeOpera))
-				   if (params.civico)
-				   		eq('ncivnume', params.civico)   
-				   if (params.barra)
-				   		eq('ncivbarr', params.barra)   
-			  }
-		*/	  			  
-			}  
+                /*		   
+                indirizzo {
+                eq('cviacodi', new Long(params.sedeOpera))
+                if (params.civico)
+                eq('ncivnume', params.civico)   
+                if (params.barra)
+                eq('ncivbarr', params.barra)   
+                }
+                 */	  			  
+            }  
 
-			if (params.comuneCatastale && params.comuneCatastale.isNumber()) {
-				def restriction = "exists (select id from detail_lage_parzellen par where par.ba_id = {alias}.ba_id AND par.katastalgem_Id = $params.comuneCatastale"		
+            if (params.comuneCatastale && params.comuneCatastale.isNumber()) {
+                def restriction = "exists (select id from detail_lage_parzellen par where par.ba_id = {alias}.ba_id AND par.katastalgem_Id = $params.comuneCatastale"		
 				
-				/*
-				particelle {
-					 eq('katastalgemId', new Long(params.comuneCatastale))
-				}
-				*/
+                /*
+                particelle {
+                eq('katastalgemId', new Long(params.comuneCatastale))
+                }
+                 */
 				
-				if (params.pf)
-					restriction += " AND par.parztyp_id = 2 AND par.nummer = '${params.pf.toUpperCase().replaceAll('\'', '')}'"
+                if (params.pf)
+                restriction += " AND par.parztyp_id = 2 AND par.nummer = '${params.pf.toUpperCase().replaceAll('\'', '')}'"
 				
-				if (params.ped)
-			   	    restriction += " AND par.parztyp_id = 1 AND par.nummer = '${params.ped.toUpperCase().replaceAll('\'', '')}'"
+                if (params.ped)
+                restriction += " AND par.parztyp_id = 1 AND par.nummer = '${params.ped.toUpperCase().replaceAll('\'', '')}'"
 
-			    if (params.pm)
-					restriction += " AND par.materielleanteil = '${params.pm.toUpperCase().replaceAll('\'', '')}'"
+                if (params.pm)
+                restriction += " AND par.materielleanteil = '${params.pm.toUpperCase().replaceAll('\'', '')}'"
 					   				
-				restriction += ")"
+                restriction += ")"
 			
-				sqlRestriction restriction			
-			}
+                sqlRestriction restriction			
+            }
 			
-			if (params.cognomeRichiedente) {
-				def restriction = "exists (select id from detail_pers per where per.kennung_id = 1 AND per.ba_id = {alias}.ba_id AND per.familienname = '${params.cognomeRichiedente.toUpperCase().replaceAll('\'', '')}'"
-				if (params.nomeRichiedente)
-					restriction += " AND per.vorname = '${params.nomeRichiedente.toUpperCase().replaceAll('\'', '')}'"
-				restriction += ")".toString()
+            if (params.cognomeRichiedente) {
+                def restriction = "exists (select id from detail_pers per where per.kennung_id = 1 AND per.ba_id = {alias}.ba_id AND per.familienname = '${params.cognomeRichiedente.toUpperCase().replaceAll('\'', '')}'"
+                if (params.nomeRichiedente)
+                restriction += " AND per.vorname = '${params.nomeRichiedente.toUpperCase().replaceAll('\'', '')}'"
+                restriction += ")".toString()
 				
-				sqlRestriction restriction
+                sqlRestriction restriction
 
-			  /*
-			  persone {
-				   eq('familienname', params.cognomeRichiedente, [ignoreCase: true])
-				   if (params.nomeRichiedente)
-						   if (params.nomeRichiedente)
-				   eq('kennungId', new Long(1))
-			  }
-			  */
-			}
+                /*
+                persone {
+                eq('familienname', params.cognomeRichiedente, [ignoreCase: true])
+                if (params.nomeRichiedente)
+                if (params.nomeRichiedente)
+                eq('kennungId', new Long(1))
+                }
+                 */
+            }
 			  
-			if (params.cognomeProgettista) {
-				def restriction = "exists (select id from detail_pers per where per.kennung_id = 3 AND per.ba_id = {alias}.ba_id AND per.familienname = '${params.cognomeProgettista.toUpperCase().replaceAll('\'', '')}'"
-				if (params.nomeProgettista)
-					restriction += " AND per.vorname = '${params.nomeProgettista.toUpperCase().replaceAll('\'', '')}'"
-				restriction += ")".toString()
+            if (params.cognomeProgettista) {
+                def restriction = "exists (select id from detail_pers per where per.kennung_id = 3 AND per.ba_id = {alias}.ba_id AND per.familienname = '${params.cognomeProgettista.toUpperCase().replaceAll('\'', '')}'"
+                if (params.nomeProgettista)
+                restriction += " AND per.vorname = '${params.nomeProgettista.toUpperCase().replaceAll('\'', '')}'"
+                restriction += ")".toString()
 				
-				sqlRestriction restriction
+                sqlRestriction restriction
 
-			  /*	
-			  persone {
-				   eq('familienname', params.cognomeProgettista, [ignoreCase: true])
-				   if (params.nomeProgettista)
-				   		eq('vorname', params.nomeProgettista, [ignoreCase: true])
-				   eq('kennungId', new Long(3))
-			  }	
-			  */   
-			}  		    
+                /*	
+                persone {
+                eq('familienname', params.cognomeProgettista, [ignoreCase: true])
+                if (params.nomeProgettista)
+                eq('vorname', params.nomeProgettista, [ignoreCase: true])
+                eq('kennungId', new Long(3))
+                }	
+                 */   
+            }  		    
 		
-			if (params.sort)
-			  order(params.sort, params.order)
-			else
-			  order('sortingNummer', 'asc')
-		}
-	}
+            if (params.sort)
+            order(params.sort, params.order)
+            else
+            order('sortingNummer', 'asc')
+        }
+    }
 	
 }
